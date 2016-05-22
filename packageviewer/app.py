1f4f34121d1d699424c8c7ac30a2b7202f768140
Daniel Wilhelm <daniel@wili.li>
2016-05-22 20:21:59 +0200

    add subprocess arguments to python code

--- packageviewer/app.py
+++ packageviewer/app.py
@@ -1,6 +1,7 @@
-from gevent import monkey
+from gevent import monkey, spawn
 monkey.patch_all()
 
+import shlex, subprocess
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
@@ -11,9 +12,10 @@ socketio = SocketIO(app)
 def main():
     return render_template("index.html")
 
-@socketio.on('connect', namespace='/packages')
-def ws_connect():
-    socketio.emit('msg', 'connected', namespace='/packages')
+def yield_http_packages():
+    command = r'"C:\Program Files\Wireshark\tshark.exe" -i 3 -T fields -e frame.time -e ip.src -e http.user_agent -e http.request.method -e http.host -e http.request.uri -e urlencoded-form.key -e urlencoded-form.value -Y http.request -l'
+    args = shlex.split(command)
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 if __name__ == '__main__':
     socketio.run(app)
