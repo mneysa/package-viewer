80acc0dd453a042bae9eb39a2c2c5415b774d1fb
Dominic Keller <dokel87@gmail.com>
2016-05-23 08:46:57 +0200

    add frontend resources to repository

---
+++ packageviewer/static/util.js
@@ -0,0 +1,37 @@
+// Utils
+
+// initalize websocket
+function initSocket() {
+  var url = "http://" + document.domain + ":" + location.port;
+  var socket = io.connect(url + "/dd");
+  socket.on('dump', function(msg){
+
+    // add new dataline
+    addDataLine(msg);
+
+    // auto scroll to the end of the page
+    $(document).scrollTop($(document).height());
+  });
+}
+
+// add a new line to the table
+function addDataLine(msg) {
+  $("#requests").append(getTableLine(msg));
+}
+
+function getTableLine(msg) {
+  var parameters = msg.split(";");
+  var tableLine = "<tr>";
+
+  for (i = 0; i < parameters.length; i++) {
+      tableLine += "<td>" + parameters[i] + "</td>";
+  }
+
+  tableLine += "</tr>";
+
+  return tableLine;
+}
+
+function filterData() {
+
+}
