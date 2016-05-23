938d2a3b7d12484d70b6ae2c445fcd64e5532078
Daniel Wilhelm <daniel@wili.li>
2016-05-23 17:28:59 +0200

    implement http_receiver on webserver side

--- packageviewer/app.py
+++ packageviewer/app.py
@@ -2,8 +2,11 @@ from gevent import monkey
 monkey.patch_all()
 from gevent.server import StreamServer
 
-from multiprocessing import current_process
+from multiprocessing import Process, current_process
 from hashlib import sha256
+import socket
+import pickle
+import pyshark
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO
@@ -15,19 +18,61 @@ socketio = SocketIO(app)
 def main():
     return render_template("index.html")
 
+def http_capturer(portnum):
+    capture = pyshark.LiveCapture(interface="Wi-Fi", display_filter="http.request")
+
+    connection = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    connection.connect(('127.0.0.1', portnum))
+    connection.send(sha256(current_process().authkey).digest())
+
+    connfile = connection.makefile(mode='w')
+    pickler = pickle.Pickler(connfile, protocol=pickle.HIGHEST_PROTOCOL)
+
+    for packet in capture.sniff_continuously():
+        data = {
+            "time": str(packet.sniff_time),
+            "source": str(packet.ip.src),
+            "method": str(packet.http.request_method),
+            "url": str(packet.http.request_full_uri),
+        }
+        if hasattr(packet.http, "user_agent"):
+            data.update({"user-agent": str(packet.http.user_agent)})
+        else:
+            data.update({"user-agent": None})
+
+        if hasattr(packet, 'urlencoded-form'):
+            data.update({"params": str(packet['urlencoded-form'])})
+        else:
+            data.update({"params": None})
+
+        pickler.dump(data)
+        connfile.flush()
+
 def http_receiver(connection, address):
-    if address != '127.0.0.1':
+    (ip_addr, port) = address
+    if ip_addr != '127.0.0.1':
         connection.close()
-        print "wrong ip: " + address
         return
 
     key = connection.recv(256)
     lkey = sha256(current_process().authkey).digest()
     if key != lkey:
         connection.close()
-        print "wrong key!"
+        print "wrong key from client: " + ip_addr + ":" + str(port)
         return
+
+    unpickler = pickle.Unpickler(connection.makefile(mode='r'))
+    try:
+        while True:
+            data = unpickler.load()
+            print data
+    finally:
+        connection.close()
 
 if __name__ == '__main__':
     StreamServer('127.0.0.1:5005', handle=http_receiver).start()
+
+    capturer = Process(target=http_capturer, args=(5005,))
+    capturer.start()
+
     socketio.run(app)
