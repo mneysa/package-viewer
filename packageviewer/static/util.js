5e03afb86176f8f580aab665e1114a330063a9dd
Daniel Wilhelm <daniel@wili.li>
2016-05-25 00:30:13 +0200

    improve filtering and data extraction of packages

--- packageviewer/static/util.js
+++ packageviewer/static/util.js
@@ -7,12 +7,18 @@ function initSocket() {
   socket.on('packet', function(msg){
     console.debug(msg)
 
-    // add new dataline
-    if (isUrlOfInterest(msg.url)) {
-      addDataLine(msg);
+    if (msg.method != "M-SEARCH") {
 
-    // auto scroll to the end of the page
-      $(document).scrollTop($(document).height());
+      msg.info = filterSensitiveData(msg);
+      if (msg.info == "" && msg.params != null) {
+        msg.info = msg.params;
+      }
+
+      if (msg.info != "" || isUrlOfInterest(msg.url)) {
+        addDataLine(msg);
+        // auto scroll to the end of the page
+        $(document).scrollTop($(document).height());
+      }
     }
   });
 }
@@ -24,23 +30,32 @@ function addDataLine(msg) {
 
 function jsonToTableRow(jsonData) {
   return "<tr><td>" + jsonData.time + "</td><td>" + jsonData.source + "</td><td>" + jsonData.method + "</td><td>"
-  + filterSensitiveData(jsonData.params) + "</td><td>" + jsonData.url + "</td></tr>";
+  + jsonData.info + "</td><td>" + jsonData.url + "</td></tr>";
 }
 
-function filterSensitiveData(params) {
-  var data = "";
-  var usernames = getUsernames(params);
-  var passwords = getPasswords(params);
-  var searchUrlParams = getSearchUrlParams(params);
+function filterSensitiveData(request) {
+  var data =  "";
+  var usernames = "";
+  var passwords = "";
+  var searchFormParams = "";
+  if (request.params != null && request.params != "") {
+    usernames = getUsernames(request.params);
+    passwords = getPasswords(request.params);
+    searchFormParams = getSearchFormParams(request.params);
+  }
+  var searchUrlParams = getSearchUrlParams(request.url);
 
   if (usernames != "")
-    data = "Usernames: " + usernames + "\n";
+    data = "Usernames: " + usernames + "<br>";
 
   if (passwords != "")
-    data += "Passwords: " + passwords + "\n";
+    data += "Passwords: " + passwords + "<br>";
 
   if (searchUrlParams != "")
     data += "Search url params: " + searchUrlParams;
+
+  if (searchFormParams != "")
+    data += "Search form params: " + searchFormParams;
 
   return data;
 }
@@ -51,7 +66,7 @@ function getUsernames(params) {
   var match = userRegex.exec(params);
 
   while (match != null) {
-    usernames += match[2] + "\t";
+    usernames += match[2] + " | ";
   	match = userRegex.exec(params);
   }
 
@@ -64,7 +79,7 @@ function getPasswords(params) {
   var match = passwordRegex.exec(params);
 
   while (match != null) {
-    passwords += match[2] + "\t";
+    passwords += match[2] + " | ";
   	match = passwordRegex.exec(params);
   }
 
@@ -73,10 +88,23 @@ function getPasswords(params) {
 
 function getSearchUrlParams(params) {
   var searchUrlParams = "";
-  var searchRegex = /(?:\?|&)(?:q|query|s|search)=(.*?)(?=&|$)/img;
+  var searchRegex = /(?:\?|&)(?:q|query|s|search|was|wo|from|to)=(.*?)(?=&|$)/img;
   var match = searchRegex.exec(params);
   while (match != null) {
-    searchUrlParams += match[2] + "\t";
+    searchUrlParams += decodeURIComponent(match[1].replace(/\+/g, " ")) + " | ";
+  	match = searchRegex.exec(params);
+  }
+
+  return searchUrlParams;
+}
+
+function getSearchFormParams(params) {
+  var searchUrlParams = "";
+  var searchRegex = /"(\w*(?:query|q|search|s)\w*)"\s+=\s+"([^"]+)"\s/ig;
+  var match = searchRegex.exec(params);
+
+  while (match != null) {
+    searchUrlParams += match[2] + " | ";
   	match = searchRegex.exec(params);
   }
 
@@ -84,7 +112,7 @@ function getSearchUrlParams(params) {
 }
 
 function isUrlOfInterest(url) {
-  var urlRegex = /(?:^https?:\/\/(?:t.co\/|e.nexac.com|font|tags|pix|ad.|trc.|[^\/]*(?:cdn|ads|adnxs|advert|adingo|sharethis|scorecardresearch|lijit|wemfbox|semasio|adtechus|openx|mediamath|mathtag|addthis|adroll|mookie|symcd|nuggad|adition|pixel|tapad|track|adform|static|farm|doubleclick|yield))|(?:casalemedia|fileserv|template|modernizr|ocsp|bootstrap|jquery|adver)|\.(?:jpg|css|js|ico|gif|json|png|svg|woff2?)(?:\?|&|$))/;
+  var urlRegex = /(?:^https?:\/\/(?:t.co\/|e.nexac.com|font|tags|pix|ad.|trc.|[^\/]*(?:cdn|ads|adnxs|count|outbrain|pagead|ml314|gigya|syndic|advert|adingo|sharethis|scorecardresearch|lijit|wemfbox|semasio|adtechus|openx|mediamath|mathtag|addthis|adroll|mookie|symcd|nuggad|adition|pixel|tapad|track|adform|static|farm|doubleclick|yield))|(?:casalemedia|fileserv|template|adServer|beacon|modernizr|ocsp|bootstrap|jquery|adver)|\.(?:jpg|css|js|ico|gif|json|png|svg|woff2?)(?:\?|&|$))/;
 
   if (urlRegex.test(url)) {
   	return false;
